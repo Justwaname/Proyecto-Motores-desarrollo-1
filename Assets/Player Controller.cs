b76b7cb2aed32e3edf32a83918ab65644aa2de53
paput-78 <cuadradosimon290@gmail.com>
2026-09-10 12:59:37 -0300

    movimiento, función de correr y stamina

--- Assets/Player Controller.cs
+++ Assets/Player Controller.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using Unity.VisualScripting.InputSystem;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -6,9 +7,14 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rigidbody;
     private PlayerInput playerInput;
-    private Vector2 input;
+    private Vector2 inputWalk;
+    private float inputSprint;
 
-    public float walkSpeed = 10f;
+    [SerializeField] float currentSpeed;
+    [SerializeField] float walkSpeed = 10f;
+    [SerializeField] float runSpeed = 20f;
+    [SerializeField] float stamina = 7f;
+    [SerializeField] float timeStoppedRuning = 7f;
 
     void Start()
     {
@@ -17,11 +23,24 @@ public class PlayerController : MonoBehaviour
     }
     void Update()
     {
-        input = playerInput.actions["Move"].ReadValue<Vector2>();
-        Debug.Log(input);
+        inputWalk = playerInput.actions["Move"].ReadValue<Vector2>();
+        inputSprint = playerInput.actions["Sprint"].ReadValue<float>();
+        Debug.Log(inputWalk);
+        Debug.Log(inputSprint);
+
+        if (inputSprint != 0)
+        {
+            currentSpeed = runSpeed;
+            stamina -= 1 * Time.deltaTime;
+            timeStoppedRuning -= 1 * Time.deltaTime;
+        } else if (inputSprint == 0) { currentSpeed = walkSpeed; }
+        if (timeStoppedRuning < 7 && inputSprint == 0)
+        { timeStoppedRuning += 1 * Time.deltaTime; }
+        if(stamina < 7 && timeStoppedRuning >= 7)
+        { stamina += 1.5f * Time.deltaTime; }
     }
     private void FixedUpdate()
     {
-        rigidbody.linearVelocity = new Vector3(input.normalized.x * walkSpeed, 0, input.normalized.y * walkSpeed);  
+        rigidbody.linearVelocity = new Vector3(inputWalk.normalized.x * currentSpeed, 0, inputWalk.normalized.y * currentSpeed);  
     }
 }
